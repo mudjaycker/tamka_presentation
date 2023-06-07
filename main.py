be1f585c6a347d0ce7cde5956f87142c49e372d9
mudjaycker <tombutoyi@gmail.com>
2023-06-07 11:25:27 +0200

    Update resumé

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
 import os
 
-root = Path(__file__).parent
-templates_dir = Path(root, "templates")
+root = Path.cwd()
+templates_dir = str(Path(root, "templates").absolute())
 env = Environment(loader=FileSystemLoader(templates_dir))
-template = env.get_template("index.html")
+template = env.get_template("index.jinja")
 
 filename = Path(root, "html", "index.html")
 
@@ -13,6 +13,13 @@ filename = Path(root, "html", "index.html")
     # return "./components/"+filepath
 
 # env.globals["component"]=component
+
+colors = {
+    "blue_gradient":"linear-gradient(to bottom, #283b95, #17b2c3)",
+    "blue": "rgb(31, 185, 194)",
+    "text_white": """ style="color: white" """,
+    "text_black": """ style="color: black" """,
+}
     
 with open(filename, "w") as fh:
-    fh.write(template.render())
+    fh.write(template.render(**colors))
--- watch.py
+++ watch.py
@@ -4,13 +4,13 @@ from watchdog.events import FileSystemEventHandler
 import os
 from pathlib import Path
 
-current_path = Path(__file__).parent
+current_path = Path.cwd()
 main_file = Path(current_path, "main.py").absolute()
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
-        print(f'event type: {event.event_type}  path : {event.src_path}')
         os.system(f'python3 {main_file}')
-        print("main.py file reloaded")
+        print(f'event type: {event.event_type}  path : {event.src_path}')
+        print(f"{main_file.name} file reloaded")
 
 
 if __name__ == "__main__":
