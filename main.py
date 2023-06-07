92cc310600be6f652b29a5a9feed48f7ce039da4
mudjaycker <tombutoyi@gmail.com>
2023-06-07 13:42:54 +0200

    Correct Orthograph

--- main.py
+++ main.py
@@ -10,16 +10,17 @@ template = env.get_template("index.jinja")
 filename = Path(root, "html", "index.html")
 
 # def component(filepath: str):
-    # return "./components/"+filepath
+# return "./components/"+filepath
 
 # env.globals["component"]=component
 
 colors = {
-    "blue_gradient":"linear-gradient(to bottom, #283b95, #17b2c3)",
+    "blue_gradient": "linear-gradient(to bottom, #283b95, #17b2c3)",
     "blue": "rgb(31, 185, 194)",
     "text_white": """ style="color: white" """,
     "text_black": """ style="color: black" """,
+    "bg_yellow": """ data-background-gradient="linear-gradient(to left, #caaf34, #b16010)" style="color: white" """,
 }
-    
+
 with open(filename, "w") as fh:
     fh.write(template.render(**colors))
